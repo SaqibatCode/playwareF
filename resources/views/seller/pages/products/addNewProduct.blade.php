874750cf19e4c1b946bf16d9c72af1398e8a7c85
Areeb005 <muhammad.areeb005@gmail.com>
2024-06-30 22:08:58 +0500

    updated

--- resources/views/seller/pages/products/addNewProduct.blade.php
+++ resources/views/seller/pages/products/addNewProduct.blade.php
@@ -2798,7 +2798,7 @@
                 <label for="warranty">Check warranty</label>
                 <select name="warranty" id="warranty" class="form-control">
                     <option value="" selected>Please Select Warranty</option>
-                    
+
                 </select>
                 @error('warranty')
                     <span class="text-danger">{{ $message }}</span>
@@ -3007,36 +3007,36 @@
             })
         })
 
-        // Get Brand By Categories
-        $('#productCategory').on('change', function() {
-            let categoryID = $(this).val();
-            $.ajax({
-                url: "{{ route('seller.getBrandsByCategory') }}",
-                method: "POST",
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                data: {
-                    categoryID: categoryID
-                },
-                success: function(res) {
-                    $("#brandName").empty(); // Clear the dropdown first
-                    $('#brandName').append('<option selected>Select Brand</option>');
-                    if (res.length > 0) {
-                        res.forEach(function(brand) {
+        // // Get Brand By Categories
+        // $('#productCategory').on('change', function() {
+        //     let categoryID = $(this).val();
+        //     $.ajax({
+        //         url: "{{ route('seller.getBrandsByCategory') }}",
+        //         method: "POST",
+        //         headers: {
+        //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        //         },
+        //         data: {
+        //             categoryID: categoryID
+        //         },
+        //         success: function(res) {
+        //             $("#brandName").empty(); // Clear the dropdown first
+        //             $('#brandName').append('<option selected>Select Brand</option>');
+        //             if (res.length > 0) {
+        //                 res.forEach(function(brand) {
 
-                            $('#brandName').append('<option value="' + brand.id +
-                                '">' + brand.name + '</option>');
-                        });
-                    } else {
-                        $('#brandName').append(
-                            '<option value="">No brands available</option>');
-                    }
-                },
-                error: function(err) {
-                    console.log(err);
-                }
-            })
-        })
+        //                     $('#brandName').append('<option value="' + brand.id +
+        //                         '">' + brand.name + '</option>');
+        //                 });
+        //             } else {
+        //                 $('#brandName').append(
+        //                     '<option value="">No brands available</option>');
+        //             }
+        //         },
+        //         error: function(err) {
+        //             console.log(err);
+        //         }
+        //     })
+        // })
     </script>
 @endsection
